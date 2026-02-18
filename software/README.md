b1d0ad5e640a0e9f9cf09bb81fcbed3b38762801
Mickael Istria <mickael.istria@eclipsedevlab.com>
2026-02-18 16:56:49 +0100

    Update to Python 3
    + some refactorings to avoid usual bugs

---
+++ software/README.md
@@ -0,0 +1,10 @@
+# Software to display lights on a screen
+
+## General
+
+Reads the serial port of the Arduino through USB, and reacts to logged events by updating lights on a Canvas.
+
+## Installation
+
+1. Install tkinter (OS/distribution-specific)
+2. `pip install pyserial`
--- software/display.py
+++ software/display.py
@@ -1,28 +1,51 @@
 #!/usr/bin/python
-import Tkinter
-import tkMessageBox
+import tkinter
 import serial
 import time
 import threading
 
 class MyTkApp(threading.Thread):
+   display = None
    def __init__(self):
       threading.Thread.__init__(self)
       self.start()
    def callback(self):
       self.root.quit()
    def run(self):
-      self.top = Tkinter.Tk()
-      global display
-      display = Tkinter.Canvas(self.top, height=250, width=950)
-      display.pack()
+      self.top = tkinter.Tk()
+      self.display = tkinter.Canvas(self.top, height=250, width=950)
+      self.reset_lights()
+      self.display.pack()
       self.top.mainloop()
-      #self.root=Tkinter.Tk()
-      #self.root.protocol("WM_DELETE_WINDOW", self.callback)
-      #self.s = Tkinter.StringVar()
-      #self.s.set('Foo')#l = Tkinter.Label(self.root,textvariable=self.s)
-      #l.pack()
-      #self.root.mainloop()
+   def reset_lights(self):
+       global coord1
+       coord1 =  10, 10, 200, 200
+       global coord2
+       coord2 = 210, 10, 400, 200
+       global coord3
+       coord3 = 410, 10, 600, 200
+       global coord4
+       coord4 = 610, 10, 800, 200
+       self.on_targ_red  = self.display.create_rectangle(coord1, fill="grey")
+       self.off_targ_red = self.display.create_rectangle(coord2, fill="grey")
+       self.off_targ_grn = self.display.create_rectangle(coord3, fill="grey")
+       self.on_targ_grn  = self.display.create_rectangle(coord4, fill="grey")
+
+   def handle_message(self, s):
+       if "hitOnTargA  : 1" in s:
+           self.on_targ_red  = self.display.create_rectangle(coord1, fill="red")
+           # pause timer here
+       if "hitOffTargA  : 1" in s:
+           self.off_targ_red = self.display.create_rectangle(coord2, fill="yellow")
+           # pause timer here
+       if "hitOffTargB  : 1" in s:
+           self.off_targ_grn = self.display.create_rectangle(coord3, fill="yellow")
+           # pause timer here
+       if "hitOnTargB  : 1" in s:
+           self.on_targ_grn  = self.display.create_rectangle(coord4, fill="green")
+           # pause timer here
+       if "Reset" in s:
+           self.reset_lights()
 
 class Timer:
 
@@ -40,7 +63,7 @@ class Timer:
          self.LastTicked = NewTicked
          return diff
       else:
-         print "Cannot Tick, Timer is paused"
+         print("Cannot Tick, Timer is paused")
 
    def GetTime(self):
       if self.started is False:
@@ -59,50 +82,24 @@ class Timer:
       self.paused = False
 
 
-def reset_lights(self):
-   global coord1
-   coord1 =  10, 10, 200, 200
-   global coord2
-   coord2 = 210, 10, 400, 200
-   global coord3
-   coord3 = 410, 10, 600, 200
-   global coord4
-   coord4 = 610, 10, 800, 200
-   self.on_targ_red  = display.create_rectangle(coord1, fill="grey")
-   self.off_targ_red = display.create_rectangle(coord2, fill="grey")
-   self.off_targ_grn = display.create_rectangle(coord3, fill="grey")
-   self.on_targ_grn  = display.create_rectangle(coord4, fill="grey")
-
-
 #reset_lights()
 class serial_coms():
-   def __init__(self, device, speed, timeout):
+   app = None
+   def __init__(self, device, speed, timeout, app):
+      self.app = app
       self.ser = serial.Serial(device, speed, timeout=timeout)
       # create new timer here
-      print self.ser.portstr       # check which port was really used
+      print(self.ser.portstr)       # check which port was really used
    def wait_for_command(self):
-      reset_lights(self)
-      s = ""
+      if self.app is None or self.app.display is None:
+          return
       while 1:
-         s = self.ser.readline()        # read (timeout)
+         s = self.ser.readline().decode("utf-8")        # read (timeout)
          if s != '':
-            #print s
-            if s == 'A':
-               self.on_targ_red  = display.create_rectangle(coord1, fill="red")
-               # pause timer here
-            if s == "C":
-               self.off_targ_red = display.create_rectangle(coord2, fill="yellow")
-               # pause timer here
-            if s == "D":
-               self.off_targ_grn = display.create_rectangle(coord3, fill="yellow")
-               # pause timer here
-            if s == "B":
-               self.on_targ_grn  = display.create_rectangle(coord4, fill="green")
-               # pause timer here
-            if s == "R":
-               reset_lights(self)
+            self.app.handle_message(s)
       self.ser.close()
 
 app = MyTkApp()
-serial_coms = serial_coms('/dev/ttyUSB0', 9600, 0)
+time.sleep(1) #wait for UI loop to startup
+serial_coms = serial_coms('/dev/ttyUSB0', 9600, 0, app)
 serial_coms.wait_for_command()
